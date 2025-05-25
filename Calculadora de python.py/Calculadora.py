b1e8bea09420bf29f43261dcd5fc4b38331b6805
gabriel sousa <sousagbc@hotmail.com>
2025-05-25 17:23:51 -0300

    ajuste

--- Calculadora de python.py/Calculadora.py
+++ Calculadora de python.py/Calculadora.py
@@ -1,9 +1,27 @@
-import tkinter as tk 
-from tkinter import ttk 
+from tkinter import *
+import tkinter as TK 
 
-#janelas 
-janela = tk.tk()
-janela.title("interface grafica com header")
+root = Tk()
 
-iframe = ttk.grame(janela)
-iframe.pack(fill='x')
+class Application():
+    def __init__(self,root):
+        self.root = root
+        self.tela()
+        root.mainloop()
+
+        
+    def tela(self):
+        self.root.title("DECISOR")    
+        self.root.configure(background= 'black')
+        self.root.geometry("700x500")
+        self.root.resizable(False , False)
+        self.root.maxsize(width=1920, height=1080)
+        self.root.minsize(width=500 , height=300)
+
+if __name__ == "__main__": 
+    app = Application(root) 
+    root.mainloop() 
+       
+
+
+
