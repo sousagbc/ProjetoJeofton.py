86e6355f884b00ce9b2fda687bc3d139c483df70
gabriel sousa <sousagbc@hotmail.com>
2025-05-29 20:26:45 -0300

    Botões e cores

--- Calculadora de python.py/Calculadora.py
+++ Calculadora de python.py/Calculadora.py
@@ -3,11 +3,12 @@ import tkinter as TK
 
 #cores 
 
-cor1 = "#000000"
-cor2 = "#feffff"
-cor3 = "#38576B"
-cor4 = "#ECEFF1"
-cor5 = "#FFAB40"
+cor1 = "#0A0A0A"
+cor2 = "#1C1C1C"
+cor3 = "#E0E0E0"
+cor4 = "#FF5722"
+cor5 = "#9E9E9E"
+cor6 = "#424242"
 
 janela = Tk()
 janela.title("DECISOR")
@@ -33,6 +34,30 @@ b_3 = Button(Frame_corpo, text="/", width=5, height=2, bg=cor5, font=("Ivy 13 bo
 b_3.place(x=177, y=0)
 b_4  = Button(Frame_corpo, text="7", width=5, height=2, bg=cor2, font=("Ivy 13 bold"), fg=cor1, command=lambda: print("7"))
 b_4.place(x=0, y=52)
+b_5 = Button(Frame_corpo, text="8", width=5, height=2, bg=cor2, font=("ivy 13 bold"),fg=cor1, command = lambda: print("8") )
+b_5.place(x=59, y=52)
+b_6 = Button(Frame_corpo, text="9", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda: print("9"))
+b_6.place(x=118, y=52)
+b_7 = Button(Frame_corpo, text="*", width=5, height=2, bg=cor5, font=("ivy 13 bold"), fg=cor1, command=lambda:print("multiplicação"))
+b_7.place(x=177,y=52)
+b_8 = Button(Frame_corpo, text="4", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda:print("4"))
+b_8.place(x=0, y=104)
+b_9 = Button(Frame_corpo, text="5", width=5, height=2, bg=cor2, font=("iby 13 bold"), fg=cor1, command=lambda:print("5"))
+b_9.place(x=59, y=104)
+b_10 = Button(Frame_corpo, text="6", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg= cor1 ,command=lambda:print("6"))
+b_10.place(x=118, y=104)
+b_11 = Button(Frame_corpo, text="-", width=5, height=2, bg=cor5, font=("ivy 13 bold"), fg=cor1, command=lambda:print("subtração"))
+b_11.place(x=177, y=104)
+b_12 = Button(Frame_corpo, text="1", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda:print("1"))
+b_12.place(x=0, y=150)
+b_13 = Button(Frame_corpo, text="2", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda:print("2"))
+b_13.place(x=59, y=150)
+b_14 = Button(Frame_corpo, text="3", width=5, height=2, bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda:print("3"))
+b_14.place(x=118, y=150)
+b_15 = Button(Frame_corpo, text="+", width=5, height=2, bg=cor5, font=("ivy 13 bold"), fg=cor1, command=lambda:print("+"))
+b_15.place(x=177, y=150)
+b_16 = Button(Frame_corpo, text="0", width=11, height=3 , bg=cor2, font=("ivy 13 bold"), fg=cor1, command=lambda:print("0"))
+b_16.place(x=0, y=202)
 
 janela.mainloop()
 
